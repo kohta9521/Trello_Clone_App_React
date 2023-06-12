bf5f9206e11e091d50d4a17f047ec2220f28c79b
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-12 10:01:35 +0900

    add react froggable and froppable section

--- 4.drug/src/App.js
+++ 4.drug/src/App.js
@@ -1,9 +1,34 @@
 import "./App.css";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <div className="dragDropArea">
+      <DragDropContext>
+        <Droppable DroppableId="droppable">
+          {(provided) => (
+            <div
+              className="item"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              <Draggable draggableId="item0" index={0}>
+                {(provided) => {
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    item0
+                  </div>;
+                }}
+              </Draggable>
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
 }
 
 export default App;
