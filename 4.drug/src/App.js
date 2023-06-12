6bba957447814be5d6b8d39acfe6ab734a35bb97
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-12 10:22:50 +0900

    error not move and not catch dragabnle item about react-brautiful-dnd

--- 4.drug/src/App.js
+++ 4.drug/src/App.js
@@ -3,25 +3,21 @@ import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
 function App() {
   return (
-    <div className="dragDropArea">
+    <div>
       <DragDropContext>
-        <Droppable DroppableId="droppable">
+        <Droppable droppableId="droppable">
           {(provided) => (
-            <div
-              className="item"
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-            >
+            <div {...provided.droppableProps} ref={provided.innerRef}>
               <Draggable draggableId="item0" index={0}>
-                {(provided) => {
+                {(provided) => (
                   <div
                     ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
                   >
-                    item0
-                  </div>;
-                }}
+                    Item0
+                  </div>
+                )}
               </Draggable>
             </div>
           )}
